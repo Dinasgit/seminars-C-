b380740636cd440615555df3f5f8934a3e0d883f
dina <ms.gudial@mail.ru>
2022-11-26 17:09:24 +0700

    task 10 done

---
+++ task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// 10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 99 || n > 1000)
+{
+Console.WriteLine("Вы ввели не трёхзначное число");
+}
+else
+{
+Console.WriteLine(InCenter(n));
+}
+int ReadInt(string message)
+{
+Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{
+int result = ((a / 10) % 10);
+return result;
+}
